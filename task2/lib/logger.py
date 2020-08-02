165433afee61b649a83ac5f63386a6bb7983e4f4
xenophorus <xenophorus@gmail.com>
2020-08-02 20:55:14 +0000

    1

---
+++ task2/lib/logger.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+import time
+
+
+if 'client.py' in str(sys.argv):
+    log = logging.getLogger('client.app')
+    logname = 'client'
+else:
+    log = logging.getLogger('server.app')
+    logname = 'server'
+
+
+fmt = logging.Formatter('%(asctime)s %(levelname)s %(module)s %(message)s')
+
+fhandler = logging.FileHandler(f'log/{logname}_{time.strftime("%Y_%m_%d")}.log', encoding='utf-8')
+fhandler.level = logging.DEBUG
+fhandler.formatter = fmt
+
+shandler = logging.StreamHandler(sys.stderr)
+shandler.level = logging.ERROR
+shandler.formatter = fmt
+
+log.addHandler(fhandler)
+log.addHandler(shandler)
+log.setLevel(logging.DEBUG)
+
+log.info('logging started')
